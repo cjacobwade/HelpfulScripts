04bbfc6d02940c498b4ee9d76051a4d3a518f325
Chris Wade <cjacobwade@gmail.com>
2015-12-08 01:37:29 -0600

    Update WaitForTruthExample.cs
    Thought of an even cleaner way to write this!

--- WaitForTruthExample.cs
+++ WaitForTruthExample.cs
@@ -7,12 +7,19 @@ public class WaitForTruthExample : MonoBehaviour
 	// Also note that start can be used as a coroutine!
 	IEnumerator Start()
 	{
-		yield return StartCoroutine(WaitForTruth(() => rigidbody.velocity.magnitude > 100000000000f));
+		yield return WaitForTruth(() => rigidbody.velocity.magnitude > 100000000000f);
 		Debug.LogWarning("That's a bit excessive, isn't it?");
 		rigidbody.velocity = Vector3.zero;
 	}
 
-	protected IEnumerator WaitForTruth(Func<bool> condition)
+	// Move both of these to your own derived version of Monobehaviour and then
+	// inherit that class instead whenever you want to use WaitForTruth
+	protected Coroutine WaitForTruth(Func<bool> condition)
+	{
+		return StartCoroutine(WaitForTruthRoutine(condition));
+	}
+
+	IEnumerator WaitForTruthRoutine(Func<bool> condition)
 	{
 		while (!condition())
 			yield return null;
