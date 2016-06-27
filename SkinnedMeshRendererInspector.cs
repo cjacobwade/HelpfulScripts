4beb0906e111ee974b75f902a0400c0ec546db7b
Chris Wade <Chris Wade>
2016-06-26 23:06:57 -0500

    - Better way to edit blendshapes that adds to the default skinnedmeshrenderer component instead of requiring another one

---
+++ SkinnedMeshRendererInspector.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+[CustomEditor( typeof( SkinnedMeshRenderer ) )]
+public class SkinnedMeshRendererInspector : Editor 
+{
+	public override void OnInspectorGUI ()
+	{
+		base.OnInspectorGUI ();
+
+		SkinnedMeshRenderer skinnedMesh = (SkinnedMeshRenderer)target;
+		
+		Mesh blendMesh = skinnedMesh.sharedMesh;
+		int blendShapeCount = blendMesh.blendShapeCount;
+		
+		for( int i = 0; i < blendShapeCount; i++ )
+		{
+			float currentBlendWeight = skinnedMesh.GetBlendShapeWeight( i );
+			float setBlendWeight = EditorGUILayout.Slider( blendMesh.GetBlendShapeName( i ), currentBlendWeight, 0f, 100f );
+			skinnedMesh.SetBlendShapeWeight( i, setBlendWeight );
+		}
+	}
+}
