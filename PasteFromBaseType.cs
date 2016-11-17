956c9952e92518009734e271dfeae15f0933f256
Chris Wade <cjacobwade@gmail.com>
2016-11-17 15:59:02 -0600

    Whoops I guess this has been broken for any child types with new fields...Fixed that and renames to be more clear

--- PasteFromBaseType.cs
+++ PasteFromBaseType.cs
@@ -4,13 +4,13 @@ using UnityEngine;
 using System.Linq;
 using System.Reflection;
 
-public static class CopyFromBaseType
+public static class PasteFromBaseType
 {
-	[MenuItem("CONTEXT/Component/Copy Values From Base Type", true)]
-	static bool ValidateCopyFromInheritedType(MenuCommand menuCommand)
+	[MenuItem("CONTEXT/Component/Paste Values From Base Type", true)]
+	static bool ValidatePasteFromInheritedType(MenuCommand menuCommand)
 	{
 		var clickedComponent = menuCommand.context as Component;
-		if(clickedComponent)
+		if (clickedComponent)
 		{
 			UnityEngine.Component[] components = clickedComponent.gameObject.GetComponents(typeof(MonoBehaviour));
 			UnityEditorInternal.ComponentUtility.PasteComponentAsNew(clickedComponent.gameObject);
@@ -26,8 +26,8 @@ public static class CopyFromBaseType
 		return false;
 	}
 
-	[MenuItem("CONTEXT/Component/Copy Values From Base Type")]
-	static void CopyFromInheritedType(MenuCommand menuCommand)
+	[MenuItem("CONTEXT/Component/Paste Values From Base Type")]
+	static void PasteFromInheritedType(MenuCommand menuCommand)
 	{
 		var clickedComponent = menuCommand.context as Component;
 
@@ -35,7 +35,7 @@ public static class CopyFromBaseType
 		UnityEditorInternal.ComponentUtility.PasteComponentAsNew(clickedComponent.gameObject);
 		var tempPasteComponent = clickedComponent.gameObject.GetComponents(typeof(MonoBehaviour)).Where(c => !components.Contains(c)).First();
 
-		Type type = clickedComponent.GetType();
+		Type type = tempPasteComponent.GetType();
 		while (type != null)
 		{
 			FieldInfo[] myObjectFields = type.GetFields(
