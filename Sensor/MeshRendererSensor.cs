cdd404c6194aa37a3fe2af5c3b5f0c63502cc7b8
Chris Wade <cjacobwade@gmail.com>
2016-11-26 03:16:54 -0600

    Update MeshRendererSensor.cs

--- Sensor/MeshRendererSensor.cs
+++ Sensor/MeshRendererSensor.cs
@@ -2,6 +2,8 @@
 using System.Collections;
 using System.Collections.Generic;
 
+// This is an example use of Sensors. Pop this component onto a gameObject
+// along with a trigger collider and rigidbody and you can start using it
 public class MeshRendererSensor : Sensor<MeshRenderer>
 {
 }
