3dc33d59da9572660d808d6ea33c2efe5d137db6
Chris <cjacobwade@gmail.com>
2016-11-26 03:13:47 -0600

    - Add sensor utility which genericizes common behaviour of managing a list of things that enter/exit a trigger

---
+++ Sensor/MeshRendererSensor.cs
@@ -0,0 +1,7 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class MeshRendererSensor : Sensor<MeshRenderer>
+{
+}
---
+++ Sensor/Sensor.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Sensor<T> : MonoBehaviour where T : MonoBehaviour
+{
+	// We cache colliders of sensed objects so we can skip getcomponents on things we've already hit
+	protected Dictionary<Collider, T> _colliderToSensedMap = new Dictionary<Collider, T>();
+
+	protected List<T> _sensedTs = new List<T>();
+	public List<T> GetSensed()
+	{
+		// This verifies the contents of our sensed list before passing it on
+		// Needed for when gameobjects are destroyed while in our sensor
+		for(int i = 0; i < _sensedTs.Count; i++)
+		{
+			if(!_sensedTs[i])
+				_sensedTs.RemoveAt(i--);
+		}
+
+		return _sensedTs;
+	}
+
+	// Subscribe to these callbacks to be notified when something is sensed/unsensed
+	public System.Action<T> SensedCallback = delegate
+	{
+	};
+	public System.Action<T> UnsensedCallback = delegate
+	{
+	};
+
+	void OnTriggerEnter(Collider collider)
+	{
+		T hitT = null;
+		if(_colliderToSensedMap.ContainsKey(collider))
+			hitT = _colliderToSensedMap[collider];
+		else
+		{
+			if(collider.attachedRigidbody)
+				hitT = collider.attachedRigidbody.GetComponent<T>();
+			else
+				hitT = collider.GetComponent<T>();
+
+			// Cache even null hitT's so we don't have to check them again
+			_colliderToSensedMap.Add(collider, hitT);
+		}
+
+		if(hitT)
+		{
+			_sensedTs.Add(hitT);
+			SensedCallback(hitT);
+		}
+	}
+
+	void OnTriggerExit(Collider collider)
+	{
+		if(_colliderToSensedMap.ContainsKey(collider))
+		{
+			T unhitT = _colliderToSensedMap[collider];
+			if(unhitT)
+			{
+				_sensedTs.Remove(_colliderToSensedMap[collider]);
+				UnsensedCallback(unhitT);
+			}
+		}
+	}
+}
